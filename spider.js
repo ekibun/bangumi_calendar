6a15763348b5fbf57e345d48d1c436455e917d74
ekibun <soekibun@gmail.com>
2019-01-18 14:54:23 +0800

    init

---
+++ spider.js
@@ -0,0 +1,44 @@
+const request = require('request-promise-native');
+const git = require('simple-git')(`./`);
+const fs = require('fs');
+
+let fileName = `calendar.json`;
+
+(async () => {
+    fs.writeFileSync(fileName, `[`);
+    let date = new Date();
+    let weeklist = await request(`https://bgmlist.com/tempapi/bangumi/${date.getFullYear()}/${date.getMonth() + 1}/json`, { json: true });
+    let first = true;
+    for (id in weeklist) {
+        let bgmId = weeklist[id].bgmId;
+        let subject = undefined;
+        console.log(bgmId);
+        while (!subject)
+            subject = await request(`https://api.bgm.tv/subject/${bgmId}?responseGroup=large`, { json: true });
+        if (!subject.eps) continue;
+        let eps = [];
+        subject.eps.filter(ep => ep.airdate).forEach(ep => {
+            let lag = (new Date(ep.airdate) - date) / 1000 / 60 / 60 / 24;
+            if (Math.abs(lag) < 10)
+                eps.push(ep);
+        })
+        if (eps.length) {
+            if (!first)
+                fs.appendFileSync(fileName, `,\n`);
+            first = false;
+            fs.appendFileSync(fileName, JSON.stringify({
+                name: subject.name,
+                name_cn: subject.name_cn,
+                air_date: subject.air_date,
+                weekDayJP: weeklist[id].weekDayJP,
+                weekDayCN: weeklist[id].weekDayCN,
+                timeJP: weeklist[id].timeJP,
+                timeCN: weeklist[id].timeCN,
+                image: subject.images.grid,
+                eps: eps
+            }))
+        }
+    }
+})().then(() => {
+    fs.appendFileSync(fileName, `]`);
+});
